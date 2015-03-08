ea3427c1c605b5a5575e5a1c1edb48b407007431
mickey3nuts <giantfilthy@gmail.com>
2015-03-07 20:22:06 -0500

    .

--- scripts/zones/Abyssea-Empyreal_Paradox/MobIDs.lua
+++
@@ -1,6 +0,0 @@
------------------------------------
---  Area: Abyssea - Empyreal Paradox (255)
--- Comments: -- posX, posY, posZ
--- (Taken from 'mob_spawn_points' table)
------------------------------------
-
